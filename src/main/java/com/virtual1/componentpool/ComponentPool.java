87a080a7c151a61180393727446916404c4dddfa
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2017-04-20 17:30:37 +0300

    Revert: lock

--- src/main/java/com/virtual1/componentpool/ComponentPool.java
+++ src/main/java/com/virtual1/componentpool/ComponentPool.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by misha on 18.04.17.
@@ -19,8 +18,6 @@ public class ComponentPool<K, V> {
     private final long timeToLive;
     private final PoolCleaner cleaner;
 
-    private AtomicBoolean isLocked = new AtomicBoolean(false);
-
     ComponentPool(String name, Long timeToLive) {
         this.name = name;
         this.timeToLive = timeToLive == null ? PoolConfig.getTimeToLive() : timeToLive;
@@ -28,46 +25,29 @@ public class ComponentPool<K, V> {
     }
 
     public synchronized V get(K key) {
-        try {
-            waitForUnlock();
-            lock();
-            ComponentWrapper<V> wrapper = registry.get(key);
-            if (wrapper != null) {
-                wrapper.updateLastAccess();
-                return wrapper.getData();
-            } else {
-                return null;
-            }
-        } finally {
-            unlock();
+        ComponentWrapper<V> wrapper = registry.get(key);
+        if (wrapper != null) {
+            wrapper.updateLastAccess();
+            return wrapper.getData();
+        } else {
+            return null;
         }
     }
 
-    public synchronized V putIfAbsent(K key, V value) {
-        try {
-            waitForUnlock();
-            lock();
-            ComponentWrapper<V> wrapper = registry.get(key);
-            if (wrapper == null) {
-                wrapper = new ComponentWrapper<>(value);
-                registry.put(key, wrapper);
-            } else {
-                wrapper.updateLastAccess();
-            }
-            return wrapper.getData();
-        } finally {
-            unlock();
+
+    public V putIfAbsent(K key, V value) {
+        ComponentWrapper<V> wrapper = registry.get(key);
+        if (wrapper == null || (value != null && !value.equals(wrapper.getData()))) {
+            wrapper = new ComponentWrapper<>(value);
+            registry.put(key, wrapper);
+        } else {
+            wrapper.updateLastAccess();
         }
+        return wrapper.getData();
     }
 
     public synchronized void remove(K key) {
-        try {
-            waitForUnlock();
-            lock();
-            registry.remove(key);
-        } finally {
-            unlock();
-        }
+        registry.remove(key);
     }
 
     public String getName() {
@@ -75,49 +55,24 @@ public class ComponentPool<K, V> {
     }
 
     public void destroy() {
-        try {
-            waitForUnlock();
-            lock();
-            cleaner.interrupt();
-            registry.clear();
-        } finally {
-            unlock();
-        }
+        cleaner.interrupt();
+        registry.clear();
     }
 
     synchronized void cleanExpiredElements() {
-        try {
-            waitForUnlock();
-            lock();
-            Set<K> toRemove = new HashSet<>();
-            long validAfter = System.currentTimeMillis() - timeToLive;
-            for (Map.Entry<K, ComponentWrapper<V>> entry : registry.entrySet()) {
-                long exceedTime = validAfter - entry.getValue().getLastAccess();
-                if (exceedTime >= 0) {
-                    K key = entry.getKey();
-                    LOGGER.trace("Component under key " + key + " in the " + name + " pool has been expired, exceed time: " + exceedTime + " ms");
-                    toRemove.add(key);
-                }
+        Set<K> toRemove = new HashSet<>();
+        long validAfter = System.currentTimeMillis() - timeToLive;
+        for (Map.Entry<K, ComponentWrapper<V>> entry : registry.entrySet()) {
+            long exceedTime = validAfter - entry.getValue().getLastAccess();
+            if (exceedTime >= 0) {
+                K key = entry.getKey();
+                LOGGER.trace("Component under key " + key + " in the " + name + " pool has been expired, exceed time: " + exceedTime + " ms");
+                toRemove.add(key);
             }
-
-            for (K k : toRemove) {
-                registry.remove(k);
-            }
-        } finally {
-            unlock();
         }
-    }
 
-    private void lock() {
-        isLocked.set(true);
-    }
-
-    private void unlock() {
-        isLocked.set(false);
-    }
-
-    private void waitForUnlock() {
-        while (isLocked.get()) {
+        for (K k : toRemove) {
+            registry.remove(k);
         }
     }
 
